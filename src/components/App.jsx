0581fc3b7c4a32aef6582eacd1551810cdcdca74
ira089 <gvir2015@gmail.com>
2024-07-18 16:53:04 +0300

    toastUpd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 import React from 'react';
 import  Layout  from '../Pages/Layout/Layout';
-// import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const TeachersPage = lazy(() => import('../Pages/TeachersPage/TeachersPage'));
@@ -11,25 +11,25 @@ const HomePage = lazy(() => import('../Pages/HomePage/HomePage'))
 
 const App = () => {
   return (
-    <Routes>
-    <Route path="/" element={<Layout />}>
-       <Route index element={<HomePage />} />
-       <Route path="/teachers" element={<TeachersPage />} />
-       <Route path="/favorites" element={<FavoritesPage />} />
-       <Route path="*" element={<HomePage />}/>
-    </Route>
- </Routes>
-    // <>
-    //    <Routes>
-    //       <Route path="/" element={<Layout />}>
-    //          <Route index element={<HomePage />} />
-    //          <Route path="/teachers" element={<TeachersPage />} />
-    //          <Route path="/favorites" element={<FavoritesPage />} />
-    //          <Route path="*" element={<HomePage />}/>
-    //       </Route>
-    //    </Routes>
-    //    <ToastContainer autoClose={3000} />
-    // </>
+//     <Routes>
+//     <Route path="/" element={<Layout />}>
+//        <Route index element={<HomePage />} />
+//        <Route path="/teachers" element={<TeachersPage />} />
+//        <Route path="/favorites" element={<FavoritesPage />} />
+//        <Route path="*" element={<HomePage />}/>
+//     </Route>
+//  </Routes>
+    <>
+       <Routes>
+          <Route path="/" element={<Layout />}>
+             <Route index element={<HomePage />} />
+             <Route path="/teachers" element={<TeachersPage />} />
+             <Route path="/favorites" element={<FavoritesPage />} />
+             <Route path="*" element={<HomePage />}/>
+          </Route>
+       </Routes>
+       <ToastContainer autoClose={3000} />
+    </>
     
   );
 };
