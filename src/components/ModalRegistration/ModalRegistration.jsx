e0c6c6a375d9e9d5165203d801ef07c51b1bed6a
ira089 <gvir2015@gmail.com>
2024-07-18 17:46:38 +0300

    toastregistr

--- src/components/ModalRegistration/ModalRegistration.jsx
+++ src/components/ModalRegistration/ModalRegistration.jsx
@@ -9,7 +9,7 @@ import { BiHide } from "react-icons/bi";
 import Button from 'components/Button/Button';
 import styles from '../ModalLogIn/ModalLogIn.module.css'
 import {auth} from '../../firebase/firebase'
-// import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
 
  const ModalRegistration = (onClose) => {
@@ -41,10 +41,10 @@ import {auth} from '../../firebase/firebase'
                   userId: user.uid,
                   token: user.accessToken,
                 }))
-                // toast.success('Congratulations! You have successfully registered!');
+                toast.success('Congratulations! You have successfully registered!');
                })
-              //  .catch(() => toast.error('Invalid user!'))
-              .catch(() => alert('Invalid user!'))
+               .catch(() => toast.error('Invalid user!'))
+              // .catch(() => alert('Invalid user!'))
                ;
                    
     reset();
